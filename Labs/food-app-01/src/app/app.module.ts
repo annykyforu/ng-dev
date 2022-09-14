b0b7595147cd3e24a4fb9910410b84066b65fc78
Andrii Nykyforuk <andriy.nb@me.com>
2022-09-14 13:30:14 +0200

    L03: added Material Design

--- Labs/food-app-01/src/app/app.module.ts
+++ Labs/food-app-01/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+
 import { NavbarComponent } from "./shared/navbar/navbar.component";
 import { SidemenuComponent } from "./shared/sidemenu/sidemenu.component";
 import { HomeComponent } from "./home/home.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HttpClientModule } from "@angular/common/http";
 import { AboutComponent } from './about/about.component';
 import { FoodContainerComponent } from './food/food-container/food-container.component';
 import { FoodListComponent } from './food/food-list/food-list.component';
@@ -29,6 +32,8 @@ import { FoodEditComponent } from './food/food-edit/food-edit.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    MatToolbarModule,
+    FlexLayoutModule
   ],
   bootstrap: [AppComponent],
 })
