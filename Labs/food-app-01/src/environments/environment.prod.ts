5d9f340d4fb4b8650fed3de3bb9b401583ca54a7
Andrii Nykyforuk <18334176+annykyforu@users.noreply.github.com>
2022-09-16 12:22:41 +0200

    added firebase

--- Labs/food-app-01/src/environments/environment.prod.ts
+++ Labs/food-app-01/src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "assets/menuItems.json",
+
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig: {
+    apiKey: "AIzaSyD-YM88S6bV50QHjzKW9JYUEaBaYBx0bB8",
+    authDomain: "angular-foodapp-etc.firebaseapp.com",
+    projectId: "angular-foodapp-etc",
+    storageBucket: "angular-foodapp-etc.appspot.com",
+    messagingSenderId: "369289237329",
+    appId: "1:369289237329:web:477ec59281d7578f66b970",
+    measurementId: "G-4JFN9GDYK3"
+  }
 };
